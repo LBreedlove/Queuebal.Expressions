118f315e9d6659d4003bbcf61b052833ab44be2c
Lucas Breedlove <LBreedlove@gmail.com>
2025-06-23 06:17:01 -0500

    Add tests for StringJoinMutation

---
+++ Queuebal.UnitTests.Expressions.Mutations/TestStringJoinMutation.cs
@@ -0,0 +1,62 @@
+using Queuebal.Expressions;
+using Queuebal.Expressions.Mutations;
+using Queuebal.Json;
+
+
+namespace Queuebal.UnitTests.Expressions.Mutations;
+
+[TestClass]
+public class TestStringJoinMutation
+{
+    public TestStringJoinMutation()
+    {
+        Context = new ExpressionContext(new Json.Data.DataProvider());
+    }
+
+    /// <summary>
+    /// The ExpressionContext used in the tests.
+    /// </summary>
+    public ExpressionContext Context { get; }
+
+    [TestMethod]
+    public void test_evaluate_when_input_is_not_list_throws()
+    {
+        // Arrange
+        var mutation = new StringJoinMutation { Separator = "." };
+        var inputValue = "not a list";
+
+        // Act & Assert
+        Assert.ThrowsExactly<InvalidOperationException>(() => mutation.Evaluate(Context, inputValue));
+    }
+
+    [TestMethod]
+    public void test_evaluate_when_input_list_contains_non_string_value_throws()
+    {
+        // Arrange
+        var mutation = new StringJoinMutation { Separator = "." };
+        var inputValue = new List<JSONValue>
+        {
+            "hello", "world", 123, false, "value"
+        };
+
+        // Act & Assert
+        Assert.ThrowsExactly<InvalidOperationException>(() => mutation.Evaluate(Context, inputValue));
+    }
+
+    [TestMethod]
+    public void test_evaluate_combines_strings_with_separator()
+    {
+        // Arrange
+        var mutation = new StringJoinMutation { Separator = "." };
+        var inputValue = new List<JSONValue>
+        {
+            "hello", "world", "value"
+        };
+
+        // Act
+        var result = mutation.Evaluate(Context, inputValue);
+
+        // Assert
+        Assert.AreEqual("hello.world.value", result.StringValue);
+    }
+}
